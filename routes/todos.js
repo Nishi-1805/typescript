58bdd40412579967a1964057aa6efc17ec68d52a
Nishi-1805 <nishimishra180500@gmail.com>
2024-09-03 19:25:10 +0530

    commit

--- routes/todos.js
+++ routes/todos.js
@@ -3,9 +3,11 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 let todos = [];
 const router = (0, express_1.Router)();
+
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
+
 router.post('/todo', (req, res, next) => {
     const newTodo = {
         id: new Date().toISOString(),
