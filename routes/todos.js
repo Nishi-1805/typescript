d097d83326d276145fd85893c2b223067e828ebd
Nishi-1805 <nishimishra180500@gmail.com>
2024-09-03 19:26:20 +0530

    change

--- routes/todos.js
+++ routes/todos.js
@@ -3,7 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = require("express");
 let todos = [];
 const router = (0, express_1.Router)();
-
 router.get('/', (req, res, next) => {
     res.status(200).json({ todos: todos });
 });
